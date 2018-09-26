6c7a7826516ae6fea4cf5a237ee3d5b147035d90
Maciej <gorski.maciej@o2.pl>
2018-09-26 21:53:53 +0200

    fixed a duration bug

--- Source Codes/Program.cs
+++ Source Codes/Program.cs
@@ -19,7 +19,10 @@ namespace otusznje {
             pan_Jezus,
             nie_wiem_choc_sie_domyslam,
             zaklecie_korwina,
-            to_be_continued
+            to_be_continued,
+            OOF,
+            spok_co_do_kurwy,
+            usrr_anthem
         }
         static void Main(string[] args) {
             SoundPlayer benin = null;
@@ -28,9 +31,8 @@ namespace otusznje {
             string[] files = Directory.GetFiles(current, "*.*", SearchOption.AllDirectories);
             files = files.Where(x => Path.GetExtension(x).ToLower() == ".wav").ToArray();
             long[] durations = null;
-
             durations = files.Select(file => TagLib.File.Create(file)).
-                Select(dur => dur.Properties.Duration.Seconds * 1000 + dur.Properties.Duration.Milliseconds).
+                Select(dur => dur.Properties.Duration.TotalMilliseconds).
                 Select(val => (long)val).
                 ToArray();
             try {
@@ -41,34 +43,43 @@ namespace otusznje {
             }
             switch (succ) {
                 case Memez.otusznje:
-                    benin = new SoundPlayer("Sounds/0_otusznje.wav");
+                    benin = new SoundPlayer("Sounds/00_otusznje.wav");
                     break;
                 case Memez.jeszcze_jak:
-                    benin = new SoundPlayer("Sounds/1_jeszcze_jak.wav");
+                    benin = new SoundPlayer("Sounds/01_jeszcze_jak.wav");
                     break;
                 case Memez.dość:
-                    benin = new SoundPlayer("Sounds/2_dość.wav");
+                    benin = new SoundPlayer("Sounds/02_dość.wav");
                     break;
                 case Memez.co:
-                    benin = new SoundPlayer("Sounds/3_co.wav");
+                    benin = new SoundPlayer("Sounds/03_co.wav");
                     break;
                 case Memez.nie_wiem:
-                    benin = new SoundPlayer("Sounds/4_nie_wiem.wav");
+                    benin = new SoundPlayer("Sounds/04_nie_wiem.wav");
                     break;
                 case Memez.REEEE:
-                    benin = new SoundPlayer("Sounds/5_REEE.wav");
+                    benin = new SoundPlayer("Sounds/05_REEE.wav");
                     break;
                 case Memez.pan_Jezus:
-                    benin = new SoundPlayer("Sounds/6_takjakpanJezuspowiedzial.wav");
+                    benin = new SoundPlayer("Sounds/06_takjakpanJezuspowiedzial.wav");
                     break;
                 case Memez.nie_wiem_choc_sie_domyslam:
-                    benin = new SoundPlayer("Sounds/7_niewiemchocsiedomyslam.wav");
+                    benin = new SoundPlayer("Sounds/07_niewiemchocsiedomyslam.wav");
                     break;
                 case Memez.zaklecie_korwina:
-                    benin = new SoundPlayer("Sounds/8_zaklecie_korwina.wav");
+                    benin = new SoundPlayer("Sounds/08_zaklecie_korwina.wav");
                     break;
                 case Memez.to_be_continued:
-                    benin = new SoundPlayer("Sounds/9_to_be_continued.wav");
+                    benin = new SoundPlayer("Sounds/09_to_be_continued.wav");
+                    break;
+                case Memez.OOF:
+                    benin = new SoundPlayer("Sounds/10_OOF.wav");
+                    break;
+                case Memez.spok_co_do_kurwy:
+                    benin = new SoundPlayer("Sounds/11_spok_co_do_kurwy.wav");
+                    break;
+                case Memez.usrr_anthem:
+                    benin = new SoundPlayer("Sounds/12_usrr_anthem.wav");
                     break;
                 default:
                     return;
